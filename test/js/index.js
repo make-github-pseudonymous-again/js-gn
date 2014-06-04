4cd0173e138f258119080c4c1eb65a9772fac25c
Ooms Aurélien <aurelien.ooms@gmail.com>
2014-06-04 11:30:22 +0200

    upgrade recquire, algo

--- test/js/index.js
+++ test/js/index.js
@@ -1,5 +1,5 @@
 var recquire_t = require('recquire');
 
-var recquire = recquire_t('gn-test', 'index.js', false, false);
+var recquire = recquire_t('gn-test', 'index.js', 'intro.js', 'outro.js', false, false);
 
 recquire(__dirname + '/src/', module.exports, -1);
