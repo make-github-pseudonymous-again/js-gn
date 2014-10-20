f8758a29a31437da3d0c5fa79f9bb1f2049a2543
aureooms <aurelien.ooms@gmail.com>
2014-10-20 21:45:18 +0200

    style

--- js/dist/gn.js
+++ js/dist/gn.js
@@ -44,28 +44,35 @@ exports.dup_t = dup_t;
 /* js/src/001 undirected/offline/algo/eulerian/eventour.js */
 
 
-var eventour_t = function(){
+var eventour_t = function () {
 
 	/**
-	 * @param graph g
-	 * @param vertices V
-	 * @param index i node from where to start the search
-	 * @param flag list done to label saturated edges
-	 * @param iterator list it that stores info on already processed edges
-	 * @param list tour the output tour vertex sequence
-	 * @param list edges the output tour edges
+	 * @param {int} fjfj dkdj
+	 * @param {graph} g
+	 * @param {vertices} V
+	 * @param {index} i node from where to start the search
+	 * @param {flag list} done to label saturated edges
+	 * @param {iterator list} it that stores info on already processed edges
+	 * @param {list} tour the output tour vertex sequence
+	 * @param {list} edges the output tour edges
 	 */
 
-	var eventour = function(g, V, i, done, it, tour, edges){
-		
-		var u, j, z = [i, 0];
+	var eventour = function ( g, V, i, done, it, tour, edges ) {
 
-		var r = [];
-		
-		while(true){
+		var u, j, z, r, end;
 
-			while (done[z[0]]) {
-				if (!r.length) return;
+		z = [i, 0];
+
+		r = [];
+
+		while ( true ) {
+
+			while ( done[z[0]] ) {
+
+				if ( r.length === 0 ) {
+					return;
+				}
+
 				z = r.pop();
 			}
 
@@ -74,26 +81,38 @@ var eventour_t = function(){
 			u = V[i];
 			done[i] = true;
 
-			while(true){
-				var end = true;
-				it[i] = g.eitr(u, function(e, v){
-					if(e.free){
+			while ( true ) {
+
+				end = true;
+
+				it[i] = g.eitr( u, function ( e, v ) {
+
+					if ( e.free ) {
+
 						tour.splice(j, 0, i);
 						edges.splice(j, 0, e);
+
 						u = v;
 
 						++j;
-						if (!done[u[0]]) r.push([u[0], j]);
+
+						if ( ! done[u[0]] ) {
+							r.push( [u[0], j] );
+						}
 
 						e.free = false;
 						end = false;
 						i = u[0];
+
 						return true;
+
 					}
-				}, it[i]);
 
-				if(end) break;
+				}, it[i] );
 
+				if ( end ) {
+					break;
+				}
 
 			}
 		}
@@ -104,6 +123,7 @@ var eventour_t = function(){
 };
 
 exports.eventour_t = eventour_t;
+
 /* js/src/001 undirected/offline/algo/eulerian/oddgraph.js */
 
 
--- js/src/001 undirected/offline/algo/eulerian/eventour.js
+++ js/src/001 undirected/offline/algo/eulerian/eventour.js
@@ -1,27 +1,34 @@
 
 
-var eventour_t = function(){
+var eventour_t = function () {
 
 	/**
-	 * @param graph g
-	 * @param vertices V
-	 * @param index i node from where to start the search
-	 * @param flag list done to label saturated edges
-	 * @param iterator list it that stores info on already processed edges
-	 * @param list tour the output tour vertex sequence
-	 * @param list edges the output tour edges
+	 * @param {int} fjfj dkdj
+	 * @param {graph} g
+	 * @param {vertices} V
+	 * @param {index} i node from where to start the search
+	 * @param {flag list} done to label saturated edges
+	 * @param {iterator list} it that stores info on already processed edges
+	 * @param {list} tour the output tour vertex sequence
+	 * @param {list} edges the output tour edges
 	 */
 
-	var eventour = function(g, V, i, done, it, tour, edges){
-		
-		var u, j, z = [i, 0];
+	var eventour = function ( g, V, i, done, it, tour, edges ) {
 
-		var r = [];
-		
-		while(true){
+		var u, j, z, r, end;
 
-			while (done[z[0]]) {
-				if (!r.length) return;
+		z = [i, 0];
+
+		r = [];
+
+		while ( true ) {
+
+			while ( done[z[0]] ) {
+
+				if ( r.length === 0 ) {
+					return;
+				}
+
 				z = r.pop();
 			}
 
@@ -30,26 +37,38 @@ var eventour_t = function(){
 			u = V[i];
 			done[i] = true;
 
-			while(true){
-				var end = true;
-				it[i] = g.eitr(u, function(e, v){
-					if(e.free){
+			while ( true ) {
+
+				end = true;
+
+				it[i] = g.eitr( u, function ( e, v ) {
+
+					if ( e.free ) {
+
 						tour.splice(j, 0, i);
 						edges.splice(j, 0, e);
+
 						u = v;
 
 						++j;
-						if (!done[u[0]]) r.push([u[0], j]);
+
+						if ( ! done[u[0]] ) {
+							r.push( [u[0], j] );
+						}
 
 						e.free = false;
 						end = false;
 						i = u[0];
+
 						return true;
+
 					}
-				}, it[i]);
 
-				if(end) break;
+				}, it[i] );
 
+				if ( end ) {
+					break;
+				}
 
 			}
 		}
